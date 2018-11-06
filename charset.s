40f77984b9643e7a4e2fcb6843ec2fd20bfbe014
Steven Walter <stevenrwalter@gmail.com>
2018-11-06 15:27:31 -0500

    Reorder screenmem and charmem
    Since screenmem has a higher alignment requirement, put it first.  This makes the resulting binary
    significantly smaller
    
    zomg it even still works

--- charset.s
+++ charset.s
@@ -1,5 +1,37 @@
     .segment "CHARMEM"
     .export _CHARMEM
+    .align  256 * 8
+
+    .export _SCREENREG = (_SCREENMEM >> (2 + 4)) | (_CHARMEM >> 10) ; Use our char mem
+    ;.export _SCREENREG = ($400 >> (2 + 4)) | (_CHARMEM >> 10) ; Use our char mem with stock screen ram position
+    ;.export _SCREENREG = (_SCREENMEM >> (2 + 4)) | ($1000 >> 10) ; Use the ROM
+    ;.export _SCREENREG = _CHARMEM >> 10 ; Use our char mem with stock screen ram position
+    .export _SCREENMEM
+_SCREENMEM:
+    .res    1024
+    .align  64
+    .export _SPRITE_PTR_CARD_TOP = _SPRITE_CARD_TOP / 64
+    .export _SPRITE_CARD_TOP
+_SPRITE_CARD_TOP:
+    .incbin "images/cardsprite_top.bitmap"
+    .align  64
+
+    .export _SPRITE_PTR_CARD_BOTTOM = _SPRITE_CARD_BOTTOM / 64
+    .export _SPRITE_CARD_BOTTOM
+_SPRITE_CARD_BOTTOM:
+    .incbin "images/cardsprite_bottom.bitmap"
+    .align  64
+
+.export _SPRITE_PTR_CARD_BG = SPRITE_CARD_BG / 64
+SPRITE_CARD_BG:
+    .incbin "images/cardsprite_bg.bitmap"
+    .align  64
+
+.export _SPRITE_PTR_MOUSE = SPRITE_MOUSE / 64
+SPRITE_MOUSE:
+    .incbin "images/mouse_sprite.bitmap"
+    .align  64
+
     .align 1024
 _CHARMEM:
     .res    33*8 ; 33 chars from the ROM table will be copied here. 33rd is space (blank) character
@@ -48,34 +80,3 @@ CARD_LEFT:
     .byte   $80, $80, $80, $80, $80, $80, $80, $80
     ; solid
     .byte   $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
-    .align  256 * 8
-
-    .export _SCREENREG = (_SCREENMEM >> (2 + 4)) | (_CHARMEM >> 10) ; Use our char mem
-    ;.export _SCREENREG = ($400 >> (2 + 4)) | (_CHARMEM >> 10) ; Use our char mem with stock screen ram position
-    ;.export _SCREENREG = (_SCREENMEM >> (2 + 4)) | ($1000 >> 10) ; Use the ROM
-    ;.export _SCREENREG = _CHARMEM >> 10 ; Use our char mem with stock screen ram position
-    .export _SCREENMEM
-_SCREENMEM:
-    .res    1024
-    .align  64
-    .export _SPRITE_PTR_CARD_TOP = _SPRITE_CARD_TOP / 64
-    .export _SPRITE_CARD_TOP
-_SPRITE_CARD_TOP:
-    .incbin "images/cardsprite_top.bitmap"
-    .align  64
-
-    .export _SPRITE_PTR_CARD_BOTTOM = _SPRITE_CARD_BOTTOM / 64
-    .export _SPRITE_CARD_BOTTOM
-_SPRITE_CARD_BOTTOM:
-    .incbin "images/cardsprite_bottom.bitmap"
-    .align  64
-
-.export _SPRITE_PTR_CARD_BG = SPRITE_CARD_BG / 64
-SPRITE_CARD_BG:
-    .incbin "images/cardsprite_bg.bitmap"
-    .align  64
-
-.export _SPRITE_PTR_MOUSE = SPRITE_MOUSE / 64
-SPRITE_MOUSE:
-    .incbin "images/mouse_sprite.bitmap"
-    .align  64
